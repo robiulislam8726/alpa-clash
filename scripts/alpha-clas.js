b822c5f9fa725d17203c31d0ee54d5f145a974f2
robiulislam8726 <robiul.islam8726@gmail.com>
2024-08-22 19:53:19 +0900

    added keycolor

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -10,8 +10,21 @@
 
 //     // console.log(playgroundSection.classList)
 // }
+function continueGame(){
+    //step -1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
 
+    //set randomly generated alphabet to the show screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+
+}
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,43 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-yellow-400');
+}
+
+
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+
+
+
+
+
+
+
+
+// function getARandomAlphabet() {
+//     // get or create alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split(' ');
+//     // console.log(alphabets);
+//     // get a random index between 0-25
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+
+//     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
